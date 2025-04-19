50378fde1bdb8b781d20062d24597eff95536c3f
Lucia Heredia <luciaheredia22@gmail.com>
2025-04-19 20:31:56 +0300

    module outputs removed unnecesary

--- static_website/terraform/static-site-s3/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_name" {
-  description = "The name of the S3 bucket"
-  value = aws_s3_bucket.static_site.bucket
-}
-
-output "website_url" {
-  description = "The URL of the static website"
-  value       = aws_s3_bucket_website_configuration.static_site.website_endpoint
-}
