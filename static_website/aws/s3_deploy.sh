00a8fb2bbcd6f04879838ff027c4ddf688dbf06a
Lucia Heredia <luciaheredia22@gmail.com>
2025-04-19 20:33:03 +0300

    terraform outputs

--- static_website/aws/s3_deploy.sh
+++ static_website/aws/s3_deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-source /terraform/terraform.ftvars
 
 # === CONFIGURATION ===
 BUCKET_NAME=$(terraform -chdir=terraform output -raw bucket_name)
-REGION="$aws_region"
+REGION=$(terraform -chdir=terraform output -raw bucket_region)
 WEB_DIR="./web" 
 
-echo "Deploying static website to S3 bucket: $BUCKET_NAME"
+echo "--> Deploying static website to S3 bucket: $BUCKET_NAME"
 
 # Upload files
 aws s3 sync $WEB_DIR s3://$BUCKET_NAME --delete
 
-echo "✅ Upload complete!"
-echo "Website URL: http://$BUCKET_NAME.s3-website-$REGION.amazonaws.com"
+# Check files uploaded
 
+echo "--> ✅ Upload complete!"
+echo "--> Website URL:  http://$BUCKET_NAME.s3-website-$REGION.amazonaws.com"
