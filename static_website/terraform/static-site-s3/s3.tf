7958066b0c70acf23548a58da4d8803e3ccf719a
Lucia Heredia <luciaheredia22@gmail.com>
2025-04-19 19:32:31 +0300

    s3 policy changes

--- static_website/terraform/static-site-s3/s3.tf
+++ static_website/terraform/static-site-s3/s3.tf
@@ -1,9 +1,5 @@
 resource "aws_s3_bucket" "static_site" {
   bucket = var.bucket_name
-  
-  tags = {
-    Name = "StaticWebsite"
-  }
 }
 
 resource "aws_s3_bucket_public_access_block" "allow_public" {
@@ -18,7 +14,18 @@ resource "aws_s3_bucket_public_access_block" "allow_public" {
 resource "aws_s3_bucket_policy" "public_read" {
   bucket = aws_s3_bucket.static_site.id
 
-  policy = data.aws_iam_policy_document.website_policy.json
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.static_site.arn}/*"
+      }
+    ]
+  })
 }
 
 resource "aws_s3_bucket_website_configuration" "static_site" {
