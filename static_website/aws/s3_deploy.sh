baf469b164b0163129104a52e4ad5667117cf380
Lucia Heredia <luciaheredia22@gmail.com>
2025-04-21 23:52:56 +0300

    TODO: Check files uploaded

--- static_website/aws/s3_deploy.sh
+++ static_website/aws/s3_deploy.sh
@@ -11,6 +11,6 @@ echo "--> Deploying static website to S3 bucket: $BUCKET_NAME"
 aws s3 sync $WEB_DIR s3://$BUCKET_NAME --delete
 
 # Check files uploaded
-
-echo "--> ✅ Upload complete!"
-echo "--> Website URL:  http://$BUCKET_NAME.s3-website-$REGION.amazonaws.com"
+# TODO:
+# echo "--> ✅ Upload complete!"
+# echo "--> Website URL:  http://$BUCKET_NAME.s3-website-$REGION.amazonaws.com"
